011ea6d1939eb99d624a15e531d3476bc25d6426
Mikael Kessler <mikaelkessler@gmail.com>
2016-11-02 02:02:04 -0400

    Aggregate and build reactions with facebook service

--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -6,6 +6,29 @@ class FacebookService
     @object_id = "#{parent_id}_#{post_id}"
   end
 
+  def aggregate_reactions
+    results = get_reactions
+    build_reactions(results)
+
+    results = results.next_page
+
+    while results.present?
+      build_reactions(results)
+      results = results.next_page
+    end
+  end
+
+  def build_reactions(results)
+    results.each do |result|
+      Reaction.find_or_create_by(network_user_id: result["id"]) do |reaction|
+        reaction.network_user_link = result["link"]
+        reaction.network_user_name = result["name"]
+        reaction.network_user_picture = result["picture"]
+        reaction.category = result["type"]
+      end
+    end
+  end
+
   def get_post
     @graph.get_object("#{@object_id}?fields=from,created_time,permalink_url")
   end
