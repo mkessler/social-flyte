0e7d6109912c9a9ef30b4490feace14ad959054d
Mikael Kessler <mikaelkessler@gmail.com>
2017-01-04 00:14:43 -0500

    Add campaigns datatable

--- app/assets/javascripts/app.datatables.groala.js
+++ app/assets/javascripts/app.datatables.groala.js
@@ -2,6 +2,19 @@
   this.App || (this.App = {});
 
   App.dataTables.groala = {
+    campaigns: function() {
+      $('#groala-campaigns-table').DataTable({
+        columns: [
+          null,
+          { width: '20%' }
+        ],
+        language: {
+          info: '<small><i class="fa fa-list orange-text" aria-hidden="true"></i> Displaying _START_ - _END_ of _TOTAL_ Posts</small>'
+        },
+        processing: false,
+        serverSide: false
+      });
+    },
     posts: function() {
       $('#groala-posts-table').DataTable({
         columns: [
@@ -19,6 +32,7 @@
   }
 
   $(document).on('ready', function() {
+    App.dataTables.groala.campaigns();
     App.dataTables.groala.posts();
   });
 
