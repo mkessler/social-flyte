6b4e509ae1daf3e5d32427fd6372311c296514bf
Mikael Kessler <mikaelkessler@gmail.com>
2017-01-11 05:58:17 -0500

    Add extended title navigation

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -2,4 +2,12 @@ module OrganizationsHelper
   def organization_title(organization)
     organization.campaigns.exists? ? organization.name : 'Welcome to groala'
   end
+
+  def organization_title_class
+    current_user.organizations.count > 1 ? 'dashboard-title extended-title-nav' : 'dashboard-title'
+  end
+
+  def organization_title_nav_class
+    'extended-nav-1' if current_user.organizations.count > 1
+  end
 end
