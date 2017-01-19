ef85c5dbf93b86bc6321a7fb82f2ca698111b11f
Mikael Kessler <mikaelkessler@gmail.com>
2017-01-19 00:47:53 -0500

    Refactor JS alerts

--- app/assets/javascripts/app.posts.js
+++ app/assets/javascripts/app.posts.js
@@ -2,11 +2,21 @@
   this.App || (this.App = {});
 
   App.posts ={
+    messages: {
+      error: ['pink', 'exclamation-triangle', 'Error - There was an error trying to sync your post.'],
+      success: ['green accent-4', 'refresh fa-spin', 'Syncing - This page will automatically refresh when your sync is complete.']
+    },
     poll: function(statuses) {
       if (statuses === undefined) statuses = [];
 
       $.get(location.pathname+'/sync_status.json').done(function(data) {
         if (statuses.indexOf(data.status) < 0) location.reload(true);
+      }).error(function(data) {
+        App.utility.alert({
+          class: App.posts.messages.error[0],
+          icon: App.posts.messages.error[1],
+          message: App.posts.messages.error[2]
+        });
       }).always(function() {
         setTimeout(function() {
           App.posts.poll(statuses);
@@ -14,14 +24,20 @@
       });
     },
     sync: function() {
-      App.posts.syncAlert();
       $.post(location.pathname+'/sync_post.json').done(function(data) {
+        App.utility.alert({
+          class: App.posts.messages.success[0],
+          icon: App.posts.messages.success[1],
+          message: App.posts.messages.success[2]
+        });
         App.posts.poll(['queued', 'working']);
+      }).error(function(data) {
+        App.utility.alert({
+          class: App.posts.messages.error[0],
+          icon: App.posts.messages.error[1],
+          message: App.posts.messages.error[2]
+        });
       });
-    },
-    syncAlert: function() {
-      $('<div class="post-sync-alert col-lg-12 mb-3" style="display:none;"><div class="card card-warning text-xs-center z-depth-1"><div class="card-block"><span class="white-text"><i class="fa fa-refresh fa-spin fa-lg fa-fw"></i> Sync started! We\'ll refresh this page when your sync is complete.</span></div></div></div>').insertAfter($('.post-timestamp'));
-      $('.post-sync-alert').slideDown();
     }
   }
 
