3dce9ce9eacf1f2c077cde823490af1d6d4d4252
Mikael Kessler <mikaelkessler@gmail.com>
2017-02-11 14:49:02 -0500

    wip

---
+++ config/initializers/constants.rb
@@ -0,0 +1,14 @@
+NETWORKS = {
+  facebook: {
+    name: 'Facebook',
+    link: 'https://facebook.com/'
+  },
+  instagram: {
+    name: 'Instagram',
+    link: 'https://instagram.com/'
+  },
+  twitter: {
+    name: 'Twitter',
+    link: 'https://twitter.com/'
+  }
+}
