cab3d416062105c59489575d768a5a9e6378cb8e
Mikael Kessler <mikaelkessler@gmail.com>
2017-02-13 03:02:13 -0500

    Lock registrations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,33 +2,37 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    set_meta_tags site: meta_title('Sign Up')
-    super
+    # set_meta_tags site: meta_title('Sign Up')
+    # super
+    flash[:notice] = 'Groala is currently not accepting registrations. Please check back later.'
+    redirect_to root_url
   end
 
   # POST /resource
   def create
-    set_meta_tags site: meta_title('Sign Up')
-    build_resource(sign_up_params)
-
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        resource.process_invitation(params[:invitation_token]) if params[:invitation_token].present?
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
+    # set_meta_tags site: meta_title('Sign Up')
+    # build_resource(sign_up_params)
+    #
+    # resource.save
+    # yield resource if block_given?
+    # if resource.persisted?
+    #   if resource.active_for_authentication?
+    #     resource.process_invitation(params[:invitation_token]) if params[:invitation_token].present?
+    #     set_flash_message! :notice, :signed_up
+    #     sign_up(resource_name, resource)
+    #     respond_with resource, location: after_sign_up_path_for(resource)
+    #   else
+    #     set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+    #     expire_data_after_sign_in!
+    #     respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    #   end
+    # else
+    #   clean_up_passwords resource
+    #   set_minimum_password_length
+    #   respond_with resource
+    # end
+    flash[:notice] = 'Groala is currently not accepting registrations. Please check back later.'
+    redirect_to root_url
   end
 
   # GET /resource/edit
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,78 +1,78 @@
-require 'rails_helper'
-
-RSpec.feature 'User sign up', :type => :feature do
-  scenario 'with valid credentials' do
-    visit new_user_registration_path
-
-    first_name = Faker::Name.first_name
-    last_name = Faker::Name.last_name
-
-    fill_in 'First Name', with: first_name
-    fill_in 'Last Name', with: last_name
-    fill_in 'Email', with: Faker::Internet.free_email
-    fill_in 'Password', with: Faker::Lorem.characters(10)
-    click_button 'Submit'
-
-    expect(page).to have_text("#{first_name} #{last_name}")
-  end
-
-  scenario 'with invalid password' do
-    visit new_user_registration_path
-
-    fill_in 'First Name', with: Faker::Name.first_name
-    fill_in 'Last Name', with: Faker::Name.last_name
-    fill_in 'Email', with: Faker::Internet.free_email
-    fill_in 'Password', with: Faker::Lorem.characters(3)
-    click_button 'Submit'
-
-    expect(page).to have_text('is too short (minimum is 6 characters)')
-  end
-
-  scenario 'with empty first name' do
-    visit new_user_registration_path
-
-    fill_in 'First Name', with: ''
-    fill_in 'Last Name', with: Faker::Name.last_name
-    fill_in 'Email', with: Faker::Internet.free_email
-    fill_in 'Password', with: Faker::Lorem.characters(10)
-    click_button 'Submit'
-
-    expect(page).to have_text('can\'t be blank')
-  end
-
-  scenario 'with empty last name' do
-    visit new_user_registration_path
-
-    fill_in 'First Name', with: Faker::Name.first_name
-    fill_in 'Last Name', with: ''
-    fill_in 'Email', with: Faker::Internet.free_email
-    fill_in 'Password', with: Faker::Lorem.characters(10)
-    click_button 'Submit'
-
-    expect(page).to have_text('can\'t be blank')
-  end
-
-  scenario 'with empty email' do
-    visit new_user_registration_path
-
-    fill_in 'First Name', with: Faker::Name.first_name
-    fill_in 'Last Name', with: Faker::Name.last_name
-    fill_in 'Email', with: ''
-    fill_in 'Password', with: Faker::Lorem.characters(10)
-    click_button 'Submit'
-
-    expect(page).to have_text('can\'t be blank')
-  end
-
-  scenario 'with empty password' do
-    visit new_user_registration_path
-
-    fill_in 'First Name', with: Faker::Name.first_name
-    fill_in 'Last Name', with: Faker::Name.last_name
-    fill_in 'Email', with: Faker::Internet.free_email
-    fill_in 'Password', with: ''
-    click_button 'Submit'
-
-    expect(page).to have_text('can\'t be blank')
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.feature 'User sign up', :type => :feature do
+#   scenario 'with valid credentials' do
+#     visit new_user_registration_path
+#
+#     first_name = Faker::Name.first_name
+#     last_name = Faker::Name.last_name
+#
+#     fill_in 'First Name', with: first_name
+#     fill_in 'Last Name', with: last_name
+#     fill_in 'Email', with: Faker::Internet.free_email
+#     fill_in 'Password', with: Faker::Lorem.characters(10)
+#     click_button 'Submit'
+#
+#     expect(page).to have_text("#{first_name} #{last_name}")
+#   end
+#
+#   scenario 'with invalid password' do
+#     visit new_user_registration_path
+#
+#     fill_in 'First Name', with: Faker::Name.first_name
+#     fill_in 'Last Name', with: Faker::Name.last_name
+#     fill_in 'Email', with: Faker::Internet.free_email
+#     fill_in 'Password', with: Faker::Lorem.characters(3)
+#     click_button 'Submit'
+#
+#     expect(page).to have_text('is too short (minimum is 6 characters)')
+#   end
+#
+#   scenario 'with empty first name' do
+#     visit new_user_registration_path
+#
+#     fill_in 'First Name', with: ''
+#     fill_in 'Last Name', with: Faker::Name.last_name
+#     fill_in 'Email', with: Faker::Internet.free_email
+#     fill_in 'Password', with: Faker::Lorem.characters(10)
+#     click_button 'Submit'
+#
+#     expect(page).to have_text('can\'t be blank')
+#   end
+#
+#   scenario 'with empty last name' do
+#     visit new_user_registration_path
+#
+#     fill_in 'First Name', with: Faker::Name.first_name
+#     fill_in 'Last Name', with: ''
+#     fill_in 'Email', with: Faker::Internet.free_email
+#     fill_in 'Password', with: Faker::Lorem.characters(10)
+#     click_button 'Submit'
+#
+#     expect(page).to have_text('can\'t be blank')
+#   end
+#
+#   scenario 'with empty email' do
+#     visit new_user_registration_path
+#
+#     fill_in 'First Name', with: Faker::Name.first_name
+#     fill_in 'Last Name', with: Faker::Name.last_name
+#     fill_in 'Email', with: ''
+#     fill_in 'Password', with: Faker::Lorem.characters(10)
+#     click_button 'Submit'
+#
+#     expect(page).to have_text('can\'t be blank')
+#   end
+#
+#   scenario 'with empty password' do
+#     visit new_user_registration_path
+#
+#     fill_in 'First Name', with: Faker::Name.first_name
+#     fill_in 'Last Name', with: Faker::Name.last_name
+#     fill_in 'Email', with: Faker::Internet.free_email
+#     fill_in 'Password', with: ''
+#     click_button 'Submit'
+#
+#     expect(page).to have_text('can\'t be blank')
+#   end
+# end
