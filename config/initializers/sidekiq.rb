6b5121bedaf15a13a2714b2ed5654bedeff93e6a
Mikael Kessler <mikaelkessler@gmail.com>
2017-02-13 22:07:09 -0500

    Set up redis env variables

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: Rails.application.secrets.redis_url, namespace: "groala_sidekiq_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: "app3_sidekiq_#{Rails.env}" }
+  config.redis = { url: Rails.application.secrets.redis_url, namespace: "groala_sidekiq_#{Rails.env}" }
 end
