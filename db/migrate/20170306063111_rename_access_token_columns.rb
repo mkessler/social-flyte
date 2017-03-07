8424109a5a1cc07b1858762f88620b38cb543163
Mikael Kessler <mikaelkessler@gmail.com>
2017-03-07 02:38:53 -0500

    Remove access tokens

--- db/migrate/20170306063111_rename_access_token_columns.rb
+++
@@ -1,6 +0,0 @@
-class RenameAccessTokenColumns < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :access_tokens, :token, :encrypted_token
-    rename_column :access_tokens, :secret, :encrypted_secret
-  end
-end
--- db/migrate/20170307011523_add_iv_columns_to_access_tokens.rb
+++
@@ -1,6 +0,0 @@
-class AddIvColumnsToAccessTokens < ActiveRecord::Migration[5.0]
-  def change
-    add_column :access_tokens, :encrypted_token_iv, :string, null: false
-    add_column :access_tokens, :encrypted_secret_iv, :string
-  end
-end
