135ad9dadbb8152f5ec2a79fa8180424058037ec
mkessler <mikaelkessler@gmail.com>
2017-06-25 02:46:18 -0400

    Update favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@
 module ApplicationHelper
   def icons_meta_tags
     '<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/manifest.json">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="apple-mobile-web-app-title" content="SocialFlyte">
+    <meta name="application-name" content="SocialFlyte">
     <meta name="theme-color" content="#ffffff">'.html_safe
   end
 end
