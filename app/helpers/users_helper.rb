2ea971a55de26f4ec619ac0d66877cc176b59b58
mkessler <mikaelkessler@gmail.com>
2017-06-26 01:11:21 -0400

    Update sidebar active class methods and adjust arrow properly

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,15 +5,7 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: 'gravatar rounded-circle')
   end
 
-  def profile_sidenav_active_class(controller)
-    profile_is_active?(controller) ? 'active' : ''
-  end
-
-  def profile_sidenav_subactive_class(controller=nil, action=nil)
-    profile_is_active?(controller) && action == params[:action] ? 'active' : ''
-  end
-
-  def profile_is_active?(controller)
-    controller == params[:controller]
+  def profile_sidenav_active_class
+    params[:controller] === 'users/registrations' ? 'active' : ''
   end
 end
